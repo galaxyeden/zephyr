926537bd3913add1f952b88ee8a3298ff79699ab
Eden Uhde <eden@rainbowtree.house>
2026-01-18 01:06:13 +0000

    drivers: display: ls0xx: add Kconfig setting VCOM thread priority for ls0xx
    The ls0xx driver VCOM inversion thread has a hardcoded priority of 3,
    while in some applications this may be considered lower priority than
    time sensitive tasks, maintaining close to an even polarity balance is
    best, but this change allows developers to determine the approach.
    
    Signed-off-by: Eden Uhde <eden@rainbowtree.house>

--- drivers/display/ls0xx.c
+++ drivers/display/ls0xx.c
@@ -329,7 +329,7 @@ static int ls0xx_init(const struct device *dev)
 	/* Start thread for toggling VCOM */
 	k_tid_t vcom_toggle_tid = k_thread_create(
 		&vcom_toggle_thread, vcom_toggle_stack, K_THREAD_STACK_SIZEOF(vcom_toggle_stack),
-		ls0xx_vcom_toggle, (void *)dev, NULL, NULL, 3, 0, K_NO_WAIT);
+		ls0xx_vcom_toggle, (void *)dev, NULL, NULL, CONFIG_LS0XX_VCOM_THREAD_PRIO, 0, K_NO_WAIT);
 	k_thread_name_set(vcom_toggle_tid, "ls0xx_vcom");
 #endif /* USE_VCOM_THREAD */
 
